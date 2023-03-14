3196859a6786e8d4bcd32faecf70aaba934aba40
Brayhanzuluaga <zbaza16@gmail.com>
2023-03-13 22:51:26 -0500

    Actualizacion error url del post

--- static_example.py
+++ static_example.py
@@ -1,9 +1,10 @@
-import requests
 import json
+import requests
+
 
 # URL del endpoint para el post
 urlpost = "http://127.0.0.1:8000/event/create"
-# URL del endpoint para el get 
+# URL del endpoint para el get
 urlget = "http://127.0.0.1:8000/events"
 # Extrae los ejempos en formato json
 f = open('examples.json')
@@ -15,13 +16,15 @@ def example_create(examples: list, url: str):
         # Hacer la petición POST con los datos del evento
         response = requests.post(url, json=example)
         # Verificar si la petición fue exitosa
+        print(response.status_code)
+        print(example)
         if response.status_code == 200:
             print("La petición post fue exitosa")
         else:
             print("Ocurrió un error al hacer la petición post")
 
 
-example_create(data, urlget)
+example_create(data, urlpost)
 response = requests.get(urlget)
 # Verificar si la petición fue exitosa
 if response.status_code == 200:
