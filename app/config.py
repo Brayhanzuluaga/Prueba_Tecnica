02b31e68a3a196742c4509dede168164d04451bc
Brayhanzuluaga <zbaza16@gmail.com>
2023-03-13 22:52:05 -0500

    Cambio base de datos

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@
 # En este caso, la cadena de conexión especifica el usuario y la contraseña para acceder a la base de datos,  #noqa
 # así como la dirección del servidor y el puerto utilizado para conectarse a la base de datos.
 def get_url():
-    SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1214734456@localhost:5432/EventDB2"
+    SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1214734456@localhost:5432/EventDB3"
     return SQLALCHEMY_DATABASE_URL
