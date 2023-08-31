8a6c67bd55d2736a017bfeb43af13c88e3731b68
Brayhanzuluaga <zbaza16@gmail.com>
2023-08-31 15:28:48 -0500

    Cambio en los response e idioma de respuestas

--- app/Events/routers.py
+++ app/Events/routers.py
@@ -22,20 +22,14 @@ async def create_event_service(request: RequestEvent,
             db,
             f'El evento con id {request.parameter.id} y nombre {request.parameter.eventName} se creo correctamente',  #noqa
             "Info")
-        # return Response(
-        #     status="Ok", code="200",
-        #     message="Event created successfully").dict(exclude_none=True)
-        return {"message" : "Event created successfully"}
+        return {"message": "Event created successfully"}
     except Exception:
         db.rollback()
         repository.create_log(
             db,
             f'El evento con id {request.parameter.id} fue previemente creado',  #noqa
             "Error")
-        # return Response(
-        #     status="FAIL", code="400",
-        #     message="Event id previously created").dict(exclude_none=True)
-        return {"message" : "Event previously created"}
+        return {"message": "Event previously created"}
 
 
 # Peticion Get que devuelve una lista de eventos
@@ -50,11 +44,6 @@ async def get_event(skip: int = 0,
     """
     _events = repository.get_event(db)
     return _events
-    """
-    return Response(status="Ok",
-                    code="200",
-                    message="Success fetch all data",
-                    result=_events)"""
 
 
 # Peticion Get que devuelve una lista del log de los eventos
@@ -69,12 +58,7 @@ async def eventLogInfo(skip: int = 0,
     """
     _logs = repository.get_event_log(db)
     return _logs
-    """
-    return Response(status="Ok",
-                    code="200",
-                    message="Success fetch all data",
-                    result=_logs)
-"""
+
 
 # Peticion Get que devuelve una lista de eventos filtrados
 # clasificandolos en revisado o sin revisar
@@ -94,17 +78,16 @@ async def get_event_management(request: Request,
 
     Ejemplo de filtrado: http://127.0.0.1:8000/events/management?eventCheck=True&management=2
     """
-    if request.query_params:
-        return Response(status="Ok",
-                        code="200",
-                        message="Success fetch all data ",
-                        result=repository.get_event(db,
-                                                    **request.query_params))
+    if 0 <= int(request.query_params["management"]) <= 2:
+        if request.query_params:
+            return repository.get_event(db,
+                                        **request.query_params), request.query_params
+        else:
+            return repository.get_event(db), request.query_params
     else:
-        return Response(status="Ok",
-                        code="200",
-                        message="Success fetch all data ",
-                        result=repository.get_event(db))
+        raise HTTPException(
+            status_code=404,
+            detail="Type of management required for the event, represented by an integer (int). It can be No review (0), Management required (1), or No management (2).") #noqa
 
 
 # Peticion Get que devuelve el evento indicado mediante el id
@@ -118,17 +101,12 @@ async def get_event_id(id: int, db: Session = Depends(get_db)):
     try:
         _event = repository.get_event_by_id(db, id)
         if _event.state is False:
-            return Response(status="FAIL",
-                            code="404",
-                            message="Event previously deleted")
-        return Response(status="Ok",
-                        code="200",
-                        message="Success get  data",
-                        result=_event)
+            return {"message": "Event previously deleted"}
+        return _event
     except Exception:
         raise HTTPException(
             status_code=404,
-            detail=f"El usuario con id {id} no existe en la base de datos")
+            detail=f"The user with id {id} does not exist in the database")
 
 
 # Peticion Patch que actualiza el evento indicado.
@@ -144,7 +122,7 @@ async def update_event_service(request: RequestEventManagement,
             db.rollback()
             repository.create_log(
                 db,
-                f'Consulta del evento con id {_event.id} fallida ya que fue eliminada previamente',
+                f'Event query with id {_event.id} failed as it was previously deleted',
                 "Error")
             return Response(status="FAIL",
                             code="404",
@@ -159,7 +137,7 @@ async def update_event_service(request: RequestEventManagement,
             date=request.parameter.date,
             management=request.parameter.management)
         repository.create_log(
-            db, f'Evento de id {_event.id} actualizado correctamente', "Info")
+            db, f'Event id {_event.id} successfully updated', "Info")
         _event.id = _event.id
         return Response(status="Ok",
                         code="200",
@@ -169,11 +147,11 @@ async def update_event_service(request: RequestEventManagement,
         db.rollback()
         repository.create_log(
             db,
-            f'Actualizacion fallida, el usuario con id {request.parameter.id} no existe en la base de datos',  #noqa
+            f'Update failed, user with id {request.parameter.id} does not exist in the database',  #noqa
             "Error")
         raise HTTPException(
             status_code=404,
-            detail=f'El usuario con id {request.parameter.id} no existe en la base de datos'
+            detail=f'User with id {request.parameter.id} does not exist in the database'
         )
 
 
@@ -186,7 +164,7 @@ async def permanentlydeleted(id: int, db: Session = Depends(get_db)):
     repository.remove_event(db, event_id=id)
     repository.create_log(
         db,
-        f'El evento con id {id} fue eliminado permanentemente de manera exitosa',
+        f'The event with id {id} was successfully permanently deleted',
         "Info")
     return Response(status="Ok", code="200",
                     message="Success delete data").dict(exclude_none=True)
@@ -202,14 +180,14 @@ async def softdelete(id: int, db: Session = Depends(get_db)):
         repository.softremove_event(db, event_id=id)
     except Exception:
         repository.create_log(
-            db, f'El evento con id {id} no existe en la base de datos',
+            db, f'The event with id {id} does not exist in the database',
             "Error")
         return Response(
             status="Fail",
             code="404",
-            message=f'El evento con id {id} no existe en la base de datos'
+            message=f'The event with id {id} does not exist in the database'
         ).dict(exclude_none=True)
     repository.create_log(
-        db, f'El evento con id {id} fue eliminado de manera exitosa', "Info")
+        db, f'The event with id {id} was successfully deleted', "Info")
     return Response(status="Ok", code="200",
                     message="Success delete data").dict(exclude_none=True)
